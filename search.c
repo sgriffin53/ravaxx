a769433beb62d9a36646f5ed025a44e5fdbb5fdd
sgriffin53 <sgriffin53@gmail.com>
2019-12-07 23:28:44 +0000

    Only do LMR when cappieces <= 3

--- search.c
+++ search.c
@@ -48,10 +48,17 @@ int alphaBeta(struct position *pos, int alpha, int beta, int depth, int ply, str
 	for (int i = 0;i < num_moves;i++) {
 		makeMove(pos, &moves[i]);
 		int r = 0;
-		if (depth >= 3 && i > 25) r = 1;
-		score = -alphaBeta(pos, -beta, -alpha, depth - 1 - r, ply + 1, pv, endtime);
+		int extension = 0;
+		
+		U64 BBopppieces;
+		if (pos->tomove == O) BBopppieces = pos->Opieces;
+		else BBopppieces = pos->Xpieces;
+		int cappieces = __builtin_popcountll(BBsurrounding(moves[i].to) & BBopppieces);
+		
+		if (depth >= 3 && i > 25 && cappieces <= 3) r = 1;
+		score = -alphaBeta(pos, -beta, -alpha, depth - 1 - r + extension, ply + 1, pv, endtime);
 		if (r > 0 && score > alpha) {
-			score = -alphaBeta(pos, -beta, -alpha, depth - 1, ply + 1, pv, endtime);
+			score = -alphaBeta(pos, -beta, -alpha, depth - 1 + extension, ply + 1, pv, endtime);
 		}
 		unmakeMove(pos);
 		if (score > bestscore) {
